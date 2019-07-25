7304e81e0704082c3bb007e2a85e359f53745512
Angel <angel.angelov@inscape.tv>
2019-07-25 18:35:54 +0300

    added more tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.html import escape
 from lists.models import Item, List
 
 
@@ -49,6 +50,13 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/{}/'.format(new_list.id))
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
 
 class NewItemTest(TestCase):
 
@@ -68,7 +76,7 @@ class NewItemTest(TestCase):
     def test_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
-        response =  self.client.post(
+        response = self.client.post(
             '/lists/{}/add_item'.format(correct_list.id),
             {'item_text': 'A new item for an existing list'}
         )
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.utils.html import escape
 from lists.models import Item, List
+
+from django.core.exceptions import ValidationError
 
 
 def home_page(request):
@@ -13,7 +16,12 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {'error': error})
     return redirect('/lists/{}/'.format(list_.id))
 
 
